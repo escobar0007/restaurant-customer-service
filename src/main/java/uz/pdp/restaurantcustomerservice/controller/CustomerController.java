bc18961ce5e192950d48d15e8f3d7571a438af23
escobar <erhardo2002@mail.ru>
2024-02-12 23:43:44 +0500

    .

--- src/main/java/uz/pdp/restaurantcustomerservice/controller/CustomerController.java
+++ src/main/java/uz/pdp/restaurantcustomerservice/controller/CustomerController.java
@@ -4,8 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uz.pdp.restaurantcustomerservice.dto.CustomerUpdateDto;
 import uz.pdp.restaurantcustomerservice.service.CustomerService;
 
 @RestController
@@ -22,5 +25,10 @@ public class CustomerController {
     @GetMapping("/{id}")
     public ResponseEntity<?> getById(@PathVariable Long id) {
         return ResponseEntity.ok(customerService.getById(id));
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<?> update(@RequestBody CustomerUpdateDto customerUpdateDto) {
+        return ResponseEntity.ok(customerService.update(customerUpdateDto));
     }
 }
